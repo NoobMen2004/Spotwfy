9f5ef64c49fc560e1a2c7323155b92a368af5fdc
NoobMen2004 <syimyksartbaev10@gmail.com>
2025-08-16 22:26:34 +0600

    com3

--- account/models.py
+++ account/models.py
@@ -1,10 +1,23 @@
 from django.db import models 
 from django.contrib.auth.models import AbstractUser
 
- 
+import uuid
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
     is_active = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=300, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def create_activation_code(self):
+        code = str(uuid.uuid4())
+        self.create_activation_code
+
+    def __str__(self):
+        return self.email
+    
